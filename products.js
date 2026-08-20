e67b17a6af0451cd9aa409b8c007afd02de72f7a
Baronman <failingitup@gmail.com>
2026-08-20 13:39:56 -0400

    Square checkout site link implemented. Cannot use square checkout links for food delivery.

--- products.js
+++ products.js
@@ -8,9 +8,9 @@ const STORE = {
     {id:"custom-cakes", name:"Custom Cakes", status:"coming"}
   ],
   products: [
-    {id:"salted-chip-12", category:"cookies", name:"Salted Choc. Chip(12)", price:17.99, image:"assets/cookie_hq12.png",  orderUrl: "https://square.link/u/cjqPTlSd", description:"A smooth bite that delivers a hint of salt and fades into a rich chocolate finish."},
-    {id:"salted-chip-36", category:"cookies", name:"Salted Choc. Chip(36)", price:39.99, image:"assets/cookie_hq36.png", orderUrl: "https://square.link/u/KqNjtcEp",  description:"You're a true cookie connoisseur, and you deserve decadence. Here at last."},
-    {id:"coconut-cookies-12", category:"cookies", name:" Coconut Cookie(12)", price:14.99, image:"assets/cookie-placeholder.png", orderUrl: "https://square.link/u/xULCE9rX",  description:"Coconut flakes toasted atop a smooth, buttery cookie base."},
-    {id:"coconut-cookies-36", category:"cookies", name:" Coconut Cookie(36)", price:39.99, image:"assets/cookie-placeholder.png", orderUrl: "https://square.link/u/I49pFYJW", description:"Buttery smooth cookies topped with lightly toasted coconut flakes."}
+    {id:"salted-chip-12", category:"cookies", name:"Salted Choc. Chip(12)", price:17.99, image:"assets/cookie_hq12.png",  orderUrl: "https://cash.app/$nocrumbydayscbus/", description:"A smooth bite that delivers a hint of salt and fades into a rich chocolate finish."},
+    {id:"salted-chip-36", category:"cookies", name:"Salted Choc. Chip(36)", price:39.99, image:"assets/cookie_hq36.png", orderUrl: "https://cash.app/$nocrumbydayscbus/",  description:"You're a true cookie connoisseur, and you deserve decadence. Here at last."},
+    {id:"coconut-cookies-12", category:"cookies", name:" Coconut Cookie(12)", price:14.99, image:"assets/cookie-placeholder.png", orderUrl: "https://cash.app/$nocrumbydayscbus/",  description:"Coconut flakes toasted atop a smooth, buttery cookie base."},
+    {id:"coconut-cookies-36", category:"cookies", name:" Coconut Cookie(36)", price:39.99, image:"assets/cookie-placeholder.png", orderUrl: "https://cash.app/$nocrumbydayscbus/", description:"Buttery smooth cookies topped with lightly toasted coconut flakes."}
   ]
 };
